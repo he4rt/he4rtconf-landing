e3f6c40f066936e0be68df7ac06865101ec52733
kjkGustavo <gugumlhlimaa@gmail.com>
2021-08-31 20:27:46 -0300

    feat(refactor-sections) refactor about

---
+++ src/components/AboutSection/index.tsx
@@ -0,0 +1,36 @@
+import Title from 'components/Title'
+import Image from 'next/image'
+import about from 'assets/about.png'
+import Text from 'components/Text'
+
+import { Wrapper, ImageBox, Info } from './styles'
+
+const AboutSection = ({ ...props }) => (
+  <Wrapper {...props}>
+    <ImageBox>
+      <Image src={about} layout="fill" objectFit="cover" />
+    </ImageBox>
+    <Info>
+      <Title color="white" fontWeight={700} level={2} size="medium">
+        Sobre o evento
+      </Title>
+      <Text>
+        Muito mais que um evento, a He4rt Conf é uma iniciativa que visa
+        democratizar e facilitar o acesso a conteúdos de alta qualidade
+        produzidos pela comunidade, para a comunidade.
+      </Text>
+      <Text>
+        O evento será 100% gratuito e disponibilizara de forma pública todos os
+        materiais produzidos para a conf através do Github e redes sociais da
+        He4rt Developers.
+      </Text>
+      <Text>
+        Muito mais que um evento, a He4rt Conf é uma iniciativa que visa
+        democratizar e facilitar o acesso a conteúdos de alta qualidade
+        produzidos pela comunidade, para a comunidade.
+      </Text>
+    </Info>
+  </Wrapper>
+)
+
+export default AboutSection
