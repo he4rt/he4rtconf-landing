4f3ee63171d8a0155cac516092785caad997328c
kjkGustavo <gugumlhlimaa@gmail.com>
2021-08-31 20:28:31 -0300

    feat(refactor-sections) refactor speakers

---
+++ src/components/SpeakersSection/index.tsx
@@ -0,0 +1,54 @@
+import Title from 'components/Title'
+
+import { Wrapper, SpeakersList } from './styles'
+import SpeakerCard from 'components/SpeakerCard'
+
+const speakers = [
+  {
+    name: 'Vitor Hugo Lima',
+    stack: 'Front-end Developer no Ifood',
+    description:
+      'Front-End Developer há alguns anos, meio metido a designer e conhecido como o "Chatão do ReScript" por onde passa.',
+    image:
+      'https://cdn.discordapp.com/avatars/426540070028443688/9bdc585ce3df9ba9d2b6cb0866977876.webp'
+  },
+  {
+    name: 'Vitor Hugo Lima',
+    stack: 'Front-end Developer no Ifood',
+    description:
+      'Front-End Developer há alguns anos, meio metido a designer e conhecido como o "Chatão do ReScript" por onde passa.',
+    image:
+      'https://cdn.discordapp.com/avatars/426540070028443688/9bdc585ce3df9ba9d2b6cb0866977876.webp'
+  },
+  {
+    name: 'Vitor Hugo Lima',
+    stack: 'Front-end Developer no Ifood',
+    description:
+      'Front-End Developer há alguns anos, meio metido a designer e conhecido como o "Chatão do ReScript" por onde passa.',
+    image:
+      'https://cdn.discordapp.com/avatars/426540070028443688/9bdc585ce3df9ba9d2b6cb0866977876.webp'
+  },
+  {
+    name: 'Vitor Hugo Lim1a',
+    stack: 'Front-end Developer no Ifood',
+    description:
+      'Front-End Developer há alguns anos, meio metido a designer e conhecido como o "Chatão do ReScript" por onde passa.',
+    image:
+      'https://cdn.discordapp.com/avatars/426540070028443688/9bdc585ce3df9ba9d2b6cb0866977876.webp'
+  }
+]
+
+const SpeakersSection = ({ ...props }) => (
+  <Wrapper {...props}>
+    <Title center color="white" fontWeight={700} level={2} size="medium">
+      Palestrantes do evento
+    </Title>
+    <SpeakersList>
+      {speakers.map((speaker) => (
+        <SpeakerCard key={speaker.name} {...speaker} />
+      ))}
+    </SpeakersList>
+  </Wrapper>
+)
+
+export default SpeakersSection
