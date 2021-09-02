d98b6ada4c6e10cd246a0b3c1a19a4f360d631a4
m7he4rt <mateuspm2015@hotmail.com>
2021-09-02 15:47:39 -0300

    refactor: remove hooks

--- src/services/provider.ts
+++ src/services/provider.ts
@@ -8,6 +8,40 @@ export type DiscordProps = {
   discord_id: string
 }
 
+export type InfoProps = {
+  id: number
+  email: string
+  image: string
+  discord_id: string
+  twitch_id: string
+  discord_username: string
+  twitch_username: string
+  github_username: string
+  created_at: string
+  updated_at: string
+  messages: {
+    discord: number
+    twitch: number
+  }
+  character: {
+    id: number
+    user_id: number
+    level: number
+    current_exp: number
+    created_at: string
+    updated_at: string
+    levelup_exp: {
+      id: number
+      required: number
+      created_at: string
+      updated_at: string
+    }
+    levelup_percentage: string
+  }
+}
+
+export const fetchMe = fetch('api/userme').then((res) => res.json())
+
 export const updateUserWithTwitch = async ({
   twitch_id,
   twitch_username
