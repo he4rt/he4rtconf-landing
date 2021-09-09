9603a6c84e73d002937305cef69cfa956dbf8b01
Mateus Mendes <mateuspm2015@hotmail.com>
2021-09-09 17:07:34 -0300

    refactor: fix types on Partners

--- src/components/PartnersSection/index.tsx
+++ src/components/PartnersSection/index.tsx
@@ -4,7 +4,21 @@ import PartnerCard from 'components/PartnerCard'
 
 import { Wrapper, PartnersList } from './styles'
 
-const PartnersSection = ({ partners, ...props }: any) => (
+export type PartnersProps = {
+  id: number
+  name: string
+  tier: number
+  url: string
+  logo_path: string
+  created_at: string
+  updated_at: string
+}
+
+type PartnersSectionProps = {
+  partners: PartnersProps[]
+}
+
+const PartnersSection = ({ partners, ...props }: PartnersSectionProps) => (
   <Wrapper {...props}>
     <Title center color="white" fontWeight="bold" level={2} size="medium">
       Nossos patrocinadores
