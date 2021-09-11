b1a8e3350ffc5a8190a3c4da17bd1ff63deee5bb
kjkGustavo <gugumlhlimaa@gmail.com>
2021-09-10 22:09:52 -0300

    feat(screenshot-ticket): chrome close page

--- src/lib/screenshot.ts
+++ src/lib/screenshot.ts
@@ -1,19 +1,23 @@
-import chrome from 'chrome-aws-lambda'
 import puppeteer from 'puppeteer-core'
 
+const browserInstance = puppeteer.launch({
+  args: [],
+  dumpio: true,
+  executablePath:
+    process.platform === 'win32'
+      ? 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
+      : process.platform === 'linux'
+      ? '/usr/bin/google-chrome'
+      : '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+  headless: false
+})
+
 export default async function screenshot(url: string) {
-  console.log('print')
-  const browser = await puppeteer.launch({
-    args: [],
-    executablePath:
-      process.platform === 'win32'
-        ? 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
-        : process.platform === 'linux'
-        ? '/usr/bin/google-chrome'
-        : '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
-  })
+  const browser = await browserInstance
   const page = await browser.newPage()
   await page.setViewport({ width: 2000, height: 1000 })
   await page.goto(url)
-  return await page.screenshot({ type: 'png' })
+  const screenshotTicket = await page.screenshot({ type: 'png' })
+  await page.close()
+  return screenshotTicket
 }
