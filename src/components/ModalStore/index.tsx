ad6b474c54ba97ede419f49e14f11c73aa7c4ea5
kjkGustavo <gugumlhlimaa@gmail.com>
2021-10-02 15:49:45 -0300

    feat(store): modal to container

--- src/components/ModalStore/index.tsx
+++ src/components/ModalStore/index.tsx
@@ -1,11 +1,39 @@
 import Title from 'components/Title'
 import Image from 'next/image'
-import { Card, ImageOptions, Wrapper } from './styles'
+import { Card, ImageOptions, Wrapper, Error, Button, Input } from './styles'
 import { ProductProps } from 'components/Products'
 import { ItemKitProps } from 'components/ItemKit'
 import { useForm, Controller } from 'react-hook-form'
-import Select from 'react-select'
 import CreatableSelect from 'react-select/creatable'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+import { useState } from 'react'
+import axios from 'axios'
+import Text from 'components/Text'
+
+const schema = (limits: ItemKitProps['limits']) => {
+  return yup
+    .object({
+      fullName: yup.string().required(),
+      email: yup.string().email().required(),
+      address: yup.string().required(),
+      cep: yup.string().required(),
+      shirt: yup.string().required(),
+      bottons: yup.array().min(limits.bottons).max(limits.bottons).required(),
+      keychains: yup
+        .array()
+        .min(limits.keychains)
+        .max(limits.keychains)
+        .required(),
+      stickers: yup
+        .array()
+        .min(limits.stickers)
+        .max(limits.stickers)
+        .required(),
+      mugs: yup.array().min(limits.mugs).max(limits.mugs).required()
+    })
+    .required()
+}
 
 type ModalProps = {
   children: ReactNode
@@ -13,15 +41,35 @@ type ModalProps = {
   item: ProductProps
   itemSelected: ItemKitProps
 }
-const isValidNewOption = (inputValue, selectValue) => true
 
 const ModalStore = ({ children, item, itemSelected, ...props }: ModalProps) => {
-  const { control, handleSubmit, setValue } = useForm()
-  const onSubmit = (data) => console.log(data)
+  const {
+    control,
+    handleSubmit,
+    register,
+    formState: { errors }
+  } = useForm({
+    resolver: yupResolver(schema(itemSelected.limits))
+  })
+
+  const [disabled, setDisabled] = useState(false)
+
+  const onSubmit = async (data) => {
+    try {
+      setDisabled(true)
+      const res = await axios.post(
+        `${process.env.NEXT_PUBLIC_SITE_URL}/api/buy?itemId=${itemSelected.id}`,
+        { ...data, itemSelected }
+      )
+      window.location.href = `https://api.whatsapp.com/send?phone=558597084621&text=${res.data.messageText}`
+    } catch (e) {
+      alert('Ocorreu um erro ao realizar a compra.')
+    }
+  }
+
   return (
     <Wrapper {...props}>
       <Card>
-        {' '}
         <Title
           fontWeight="semibold"
           color="white"
@@ -32,6 +80,35 @@ const ModalStore = ({ children, item, itemSelected, ...props }: ModalProps) => {
           Finalizar compra
         </Title>
         <form onSubmit={handleSubmit(onSubmit)}>
+          <div>
+            <Title fontWeight="light" color="white" size="small" level={3}>
+              Nome completo:
+            </Title>
+            <Input {...register('fullName')} placeholder="Nome completo" />
+            <Error>{errors.fullName?.message}</Error>
+          </div>
+          <div>
+            <Title fontWeight="light" color="white" size="small" level={3}>
+              E-mail:
+            </Title>
+            <Input {...register('email')} placeholder="E-mail" />
+            <Error>{errors.email?.message}</Error>
+          </div>
+          <div>
+            <Title fontWeight="light" color="white" size="small" level={3}>
+              Endereço:
+            </Title>
+            <Input {...register('address')} placeholder="Endereço" />
+
+            <Error>{errors.address?.message}</Error>
+          </div>
+          <div>
+            <Title fontWeight="light" color="white" size="small" level={3}>
+              CEP:
+            </Title>
+            <Input {...register('cep')} placeholder="CEP" />
+            <Error>{errors.cep?.message}</Error>
+          </div>
           <div>
             <Title fontWeight="light" color="white" size="small" level={3}>
               Selecione a estampa:
@@ -47,16 +124,44 @@ const ModalStore = ({ children, item, itemSelected, ...props }: ModalProps) => {
                 />
               ))}
             </ImageOptions>
-            <Select
-              options={itemSelected.shirts.map((shirtId) => ({
-                value: item.shirts[shirtId].name,
-                label: item.shirts[shirtId].name
-              }))}
+            <Controller
+              control={control}
+              name="shirt"
+              render={({ field: { onChange, value, ref } }) => (
+                <CreatableSelect
+                  inputRef={ref}
+                  options={itemSelected.shirts.map((shirtId) => ({
+                    value: item.shirts[shirtId].name,
+                    label: item.shirts[shirtId].name
+                  }))}
+                  id="select"
+                  theme={(theme) => ({
+                    ...theme,
+                    borderRadius: 6,
+                    colors: {
+                      ...theme.colors,
+                      text: 'orangered',
+                      primary25: '#782BF1',
+                      primary: 'black',
+                      neutral0: 'black'
+                    }
+                  })}
+                  formatCreateLabel={() => undefined}
+                  value={itemSelected.shirts
+                    .map((shirtId) => ({
+                      value: item.shirts[shirtId].name,
+                      label: item.shirts[shirtId].name
+                    }))
+                    .find((c) => c.value === value)}
+                  onChange={(val) => onChange(val.value)}
+                />
+              )}
             />
+            <Error>{errors.shirt?.message}</Error>
           </div>
           <div>
             <Title fontWeight="light" color="white" size="small" level={3}>
-              Selecione 4 bottons:
+              Selecione {itemSelected.limits.bottons} bottons:
             </Title>
             <div>
               {item.bottons.map(({ name, image }) => (
@@ -75,9 +180,21 @@ const ModalStore = ({ children, item, itemSelected, ...props }: ModalProps) => {
                   <CreatableSelect
                     inputRef={ref}
                     isMulti
+                    id="select"
+                    theme={(theme) => ({
+                      ...theme,
+                      borderRadius: 6,
+                      colors: {
+                        ...theme.colors,
+                        text: 'orangered',
+                        primary25: '#782BF1',
+                        primary: 'black',
+                        neutral0: 'black'
+                      }
+                    })}
                     options={
-                      value?.length >= 3
-                        ? undefined
+                      value?.length >= itemSelected.limits.bottons
+                        ? []
                         : item.bottons.map((botton) => ({
                             value: botton.name,
                             label: botton.name
@@ -94,11 +211,12 @@ const ModalStore = ({ children, item, itemSelected, ...props }: ModalProps) => {
                   />
                 )}
               />
+              <Error>{errors.bottons?.message}</Error>
             </div>
           </div>
           <div>
             <Title fontWeight="light" color="white" size="small" level={3}>
-              Selecione 2 caveiros:
+              Selecione {itemSelected.limits.keychains} chaveiros:
             </Title>
             <div>
               {item.keychains.map(({ name, image }) => (
@@ -110,18 +228,50 @@ const ModalStore = ({ children, item, itemSelected, ...props }: ModalProps) => {
                   layout="fixed"
                 />
               ))}
-              <Select
-                isMulti
-                options={item.keychains.map((keychain) => ({
-                  value: keychain.name,
-                  label: keychain.name
-                }))}
+              <Controller
+                control={control}
+                name="keychains"
+                render={({ field: { onChange, value, ref } }) => (
+                  <CreatableSelect
+                    inputRef={ref}
+                    isMulti
+                    id="select"
+                    theme={(theme) => ({
+                      ...theme,
+                      borderRadius: 6,
+                      colors: {
+                        ...theme.colors,
+                        text: 'orangered',
+                        primary25: '#782BF1',
+                        primary: 'black',
+                        neutral0: 'black'
+                      }
+                    })}
+                    options={
+                      value?.length >= itemSelected.limits.keychains
+                        ? []
+                        : item.keychains.map((keychain) => ({
+                            value: keychain.name,
+                            label: keychain.name
+                          }))
+                    }
+                    formatCreateLabel={() => undefined}
+                    value={item.keychains
+                      .map((botton) => ({
+                        value: botton.name,
+                        label: botton.name
+                      }))
+                      .find((c) => c.value === value)}
+                    onChange={(val) => onChange(val.map((c) => c.value))}
+                  />
+                )}
               />
+              <Error>{errors.keychains?.message}</Error>
             </div>
           </div>
           <div>
             <Title fontWeight="light" color="white" size="small" level={3}>
-              Selecione 2 adesivos:
+              Selecione {itemSelected.limits.stickers} adesivos:
             </Title>
             <div>
               {item.stickers.map(({ name, image }) => (
@@ -133,18 +283,50 @@ const ModalStore = ({ children, item, itemSelected, ...props }: ModalProps) => {
                   layout="fixed"
                 />
               ))}
-              <Select
-                isMulti
-                options={item.stickers.map((sticker) => ({
-                  value: sticker.name,
-                  label: sticker.name
-                }))}
+              <Controller
+                control={control}
+                name="stickers"
+                render={({ field: { onChange, value, ref } }) => (
+                  <CreatableSelect
+                    inputRef={ref}
+                    isMulti
+                    id="select"
+                    theme={(theme) => ({
+                      ...theme,
+                      borderRadius: 6,
+                      colors: {
+                        ...theme.colors,
+                        text: 'orangered',
+                        primary25: '#782BF1',
+                        primary: 'black',
+                        neutral0: 'black'
+                      }
+                    })}
+                    options={
+                      value?.length >= itemSelected.limits.stickers
+                        ? []
+                        : item.stickers.map((sticker) => ({
+                            value: sticker.name,
+                            label: sticker.name
+                          }))
+                    }
+                    formatCreateLabel={() => undefined}
+                    value={item.stickers
+                      .map((botton) => ({
+                        value: botton.name,
+                        label: botton.name
+                      }))
+                      .find((c) => c.value === value)}
+                    onChange={(val) => onChange(val.map((c) => c.value))}
+                  />
+                )}
               />
+              <Error>{errors.stickers?.message}</Error>
             </div>
           </div>
           <div>
             <Title fontWeight="light" color="white" size="small" level={3}>
-              Selecione 1 caneca:
+              Selecione {itemSelected.limits.mugs} caneca:
             </Title>
             <div>
               {item.mugs.map(({ name, image }) => (
@@ -156,18 +338,62 @@ const ModalStore = ({ children, item, itemSelected, ...props }: ModalProps) => {
                   layout="fixed"
                 />
               ))}
-              <Select
-                isMulti
-                options={item.mugs.map((mug) => ({
-                  value: mug.name,
-                  label: mug.name
-                }))}
+              <Controller
+                control={control}
+                name="mugs"
+                render={({ field: { onChange, value, ref } }) => (
+                  <CreatableSelect
+                    inputRef={ref}
+                    isMulti
+                    id="select"
+                    theme={(theme) => ({
+                      ...theme,
+                      borderRadius: 6,
+                      colors: {
+                        ...theme.colors,
+                        text: 'orangered',
+                        primary25: '#782BF1',
+                        primary: 'black',
+                        neutral0: 'black'
+                      }
+                    })}
+                    options={
+                      value?.length >= itemSelected.limits.mugs
+                        ? []
+                        : item.mugs.map((sticker) => ({
+                            value: sticker.name,
+                            label: sticker.name
+                          }))
+                    }
+                    formatCreateLabel={() => undefined}
+                    value={item.mugs
+                      .map((botton) => ({
+                        value: botton.name,
+                        label: botton.name
+                      }))
+                      .find((c) => c.value === value)}
+                    onChange={(val) => onChange(val.map((c) => c.value))}
+                  />
+                )}
               />
+
+              <Error>{errors.mugs?.message}</Error>
             </div>
           </div>
-          <button type="submit">compra ai filho</button>
+          <Button type="submit" disabled={disabled}>
+            {disabled ? 'Carregando...' : 'COMPRAR'}
+          </Button>
+          <Text>
+            <small>
+              * A etapa final de compra será finalizada no Whatsapp.
+            </small>
+          </Text>
         </form>
       </Card>
+      <Error>* Só é possível escolher as opções informadas na descrição.</Error>
+      <Error>* O frete é definido apenas na etapa de compra.</Error>
+      <Error>* A etapa final de compra será finalizada no Whatsapp.</Error>
+      <Error>* Encomendas com início de fabricação ao fim da pré-venda.</Error>
     </Wrapper>
   )
 }
--- src/components/ModalStore/styles.ts
+++ src/components/ModalStore/styles.ts
@@ -1,24 +1,52 @@
 import styled, { css } from 'styled-components'
 import Modal from 'react-modal'
+import Text from 'components/Text'
+import Container from 'components/Container'
 
-export const Wrapper = styled(Modal)`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  right: auto;
-  bottom: auto;
-  transform: translate(-50%, -50%);
-  overflow-y: auto; // TODO tirar e mover para pagina
-`
+export const Wrapper = styled(Container)``
 
 export const Card = styled.div`
   background: #181818;
   padding: 3.5rem;
   border-radius: 0.6rem;
-  width: 600px;
 `
 
 export const ImageOptions = styled.div`
   display: flex;
   flex-wrap: wrap;
+`
+
+export const Error = styled(Text)`
+  margin-top: 1.5rem;
+  color: red;
+  a {
+    color: red;
+    font-weight: bold;
+  }
+`
+
+export const Button = styled.button`
+  background-color: #782bf1;
+  color: white;
+  font-weight: 800;
+  font-size: 18px;
+  width: 100%;
+  padding: 1.2rem;
+  border-radius: 6px;
+  border: none;
+  cursor: pointer;
+  &:disabled {
+    opacity: 0.5;
+  }
+`
+
+export const Input = styled.input`
+  width: 100%;
+  padding: 1.2rem;
+  border-radius: 6px;
+  border: none;
+  cursor: pointer;
+  background: black;
+  font-weight: 800;
+  color: white;
 `
